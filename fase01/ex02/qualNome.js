61953f66eb382e22fd0e4dc6a6b308dc9f675919
Rafaelaftsilva3 <44807901+Rafaelaftsilva3@users.noreply.github.com>
2018-11-19 12:11:26 -0200

    Rename qualnome.js to qualNome.js

---
+++ fase01/ex02/qualNome.js
@@ -0,0 +1,4 @@
+function qualNome(Rafaela) {
+ return "Meu nome é Rafaela";
+}
+console.log(qualNome("Rafaela"))
--- fase01/ex02/qualnome.js
+++
@@ -1,4 +0,0 @@
-function qualNome(Rafaela) {
- return "Meu nome é Rafaela";
-}
-console.log(qualNome("Rafaela"))
